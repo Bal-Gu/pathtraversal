a6296e5cfb263c048b9b94bb2118b27659914a3f
BalGu <guillaume@ballinger.lu>
2020-01-27 00:34:25 +0100

    only boss filter is missing

--- dockerised-php-master/code/εntry.php
+++ dockerised-php-master/code/εntry.php
@@ -9,9 +9,11 @@ if ($inputquery) {
     $inputquery = explode("#", $inputquery)[0];
 
     //prepare file for download
-    $file = "items/" . $inputquery;
+    $file = $inputquery;
 
     $filterpass = false;
+    $nullbyte = false;
+    $encode = false;
 
     //recursive filter function
     function recursivefilter ($file, $str) {
@@ -33,25 +35,24 @@ if ($inputquery) {
         $file = str_replace("REVERSESW0RD", "\\", $file);
     } else if (strpos($inputquery, "Xray.jpg") !== false) {
         //instead of really long blacklist, we whitelisted some payloads that should pass
-        if (strpos($inputquery, "%252e") !== false) {
+        //since URL decode is not recurive, this is output of double-url-encoding, whitelisted payload
+        if (strpos($inputquery, "%2e") !== false) {
             $filterpass = true;
+            $encode = true;
         } else
-        if (strpos($inputquery, "%252E") !== false) {
+        if (strpos($inputquery, "%2E") !== false) {
             $filterpass = true;
+            $encode = true;
         } else
-        if (strpos($inputquery, "%252f") !== false) {
+        if (strpos($inputquery, "%2f") !== false) {
             $filterpass = true;
+            $encode = true;
         } else
-        if (strpos($inputquery, "%252F") !== false) {
+        if (strpos($inputquery, "%2F") !== false) {
             $filterpass = true;
-        } else
-        if (strpos($inputquery, "%e0%40%ae") !== false) {
-            $filterpass = true;
-        } else
-        if (strpos($inputquery, "%e0%80%af") !== false) {
-            $filterpass = true;
+            $encode = true;
         } else {
-            die("Path Traversal attack detected. This incident will be reported.");
+            die($file);
         }
     } else if (strpos($inputquery, "Boss.jpg") !== false) {
         //no encodings here, but filters not recursive  nested payloads
@@ -65,11 +66,27 @@ if ($inputquery) {
     } else if (strpos($inputquery, "Victory.html") !== false) {
         //null byte injection attack
         $file = $file . ".txt";
+        //we had to simulate the vulnerability, since we can't get the vulnerable php version running
+        $nullbyte = true;
     } else {
         //we got it, Gandalf
         die("You shall not pass!");
     }
 
+    $file = "items/items/items/items/items/items/items/" . $file;
+    //simulate null byte injection from php 5.1.5
+    if ($nullbyte) {
+        $file = explode("%00", $file)[0];
+    }
+    
+    if ($encode) {
+        //manually decode a second time
+        $file = str_replace("%2e", ".", $file);
+        $file = str_replace("%2E", ".", $file);
+        $file = str_replace("%2f", "/", $file);
+        $file = str_replace("%2F", "/", $file);
+    }
+    
     if (!file_exists($file)) {
         http_response_code(404);
         die($file);
@@ -80,6 +97,7 @@ if ($inputquery) {
     header("Content-Type: application/octet-stream");
     readfile($file);
     die;
+
 }
 
 echo '<!DOCTYPE html>
